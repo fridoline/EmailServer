acac1445d1679076e50a8b7598734c632cfe8751
yatan001 <yatan001@stud.fh-dortmund.de>
2021-12-14 19:30:14 +0100

    main Fehler korregiert

--- src/main/java/de/emailserver/Main.java
+++ src/main/java/de/emailserver/Main.java
@@ -7,7 +7,7 @@ public class Main
 
         System.out.println("hat git funktioniert");
         System.out.println("bla bla ");
-        Sysem.out.prilntln("Test Test");
+        System.out.println("Test Test");
 
     }
 }
